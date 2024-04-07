5f28d77351b919ff16f2b6e8b6e01296671496c7
Issakov142 <82026984+Issakov142@users.noreply.github.com>
2024-04-07 14:10:19 +0500

    indexof fixed

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,7 +1,7 @@
 const words = ['яблоко', 'банан', 'апельсин', 'манго', 'киви'];
 
-if (words.indexOf('апельсин') >= 0) {
-  console.log('Ура! нашел');
+if (words.indexOf('апельсин') === 2) {
+  console.log(words.indexOf('апельсин'));
 } else {
   console.log('Придется поискать в другом магазине…');
 }
